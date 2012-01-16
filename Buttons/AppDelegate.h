270cdc3a6c581ace1a4e85586023a4e4ac783cf3
Sebastien Binet <seb@Sebastiens-iMac.local>
2012-01-16 07:36:11 -0500

    Initial Commit

---
+++ Buttons/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Buttons
+//
+//  Created by Sebastien Binet on 12-01-16.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
